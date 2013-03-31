595eda93b04c3f09267439bc2466185d1c8b4da8
Krzysztof Gabis <kgabis@gmail.com>
2013-03-31 12:32:04 +0200

    Initial Commit

---
+++ raytracer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  raytracer
+//
+//  Created by Krzysztof Gabis on 31.03.2013.
+//  Copyright (c) 2013 Krzysztof Gabis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
